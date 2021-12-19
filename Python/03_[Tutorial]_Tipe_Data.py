b390d7dbb202b5c804133f7235aadf2704ac05a0
Aszaychik <aszaychik@gmail.com>
2021-12-19 08:00:20 +0700

    init

---
+++ Python/03_[Tutorial]_Tipe_Data.py
@@ -0,0 +1,11 @@
+data_integer = 10
+print("data : ", data_integer,", tipe data : ",type(data_integer))
+
+data_float = 10.5
+print("data : ", data_float,", tipe data : ",type(data_float))
+
+data_string = "AGOES"
+print("data : ", data_string,", tipe data : ",type(data_string))
+
+data_bool = True
+print("data : ", data_bool,", tipe data : ",type(data_bool))
