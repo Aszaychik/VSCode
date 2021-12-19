9fdd97db0449686f31b035b7b329bc74845530f5
Aszaychik <aszaychik@gmail.com>
2021-12-19 08:08:17 +0700

    init

--- Python/03_[Tutorial]_Tipe_Data.py
+++ Python/03_[Tutorial]_Tipe_Data.py
@@ -9,3 +9,10 @@ print("data : ", data_string,", tipe data : ",type(data_string))
 
 data_bool = True
 print("data : ", data_bool,", tipe data : ",type(data_bool))
+
+data_complex = complex(20,116)
+print("data : ", data_complex,", tipe data : ",type(data_complex))
+
+from ctypes import c_double
+data_c_double = c_double(11.20)
+print("data : ", data_c_double,", tipe data : ",type(data_c_double))
