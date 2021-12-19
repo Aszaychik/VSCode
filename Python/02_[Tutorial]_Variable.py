84d4516cd62f3231525208be6ca2f73bf8abc577
Aszaychik <aszaychik@gmail.com>
2021-12-19 07:44:18 +0700

    init

---
+++ Python/02_[Tutorial]_Variable.py
@@ -0,0 +1,16 @@
+a = 11
+b = 16
+c = 1120
+print("Nilai a = ",a)
+print("Nilai b = ",b)
+print("Nilai c = ",c)
+
+nilai_d = 20
+nilaie = 10.10
+k100 = 100000
+
+a = 112
+print("Nilai a = ",a)
+
+b = a
+print("Nilai b = ",b)
