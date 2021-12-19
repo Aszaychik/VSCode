22877790b4a41e38f1da334a0c7279cd57ed4aae
Aszaychik <aszaychik@gmail.com>
2021-12-19 07:50:43 +0700

    init

---
+++ Python/01_[Tutorial]_Print.py
@@ -0,0 +1,11 @@
+import time
+start_time = time.time()
+
+print("NAMA : AGOES BUDI SATRIYA")
+#Comment
+"""Its Also Comment"""
+nim = 112010116
+kelas = "3B Informatika"
+print("NIM : ",nim)
+print("Kelas : ",kelas)
+print(time.time() - start_time, "Detik")
