90895662945a11d539664e1a4d414d03756234bc
Aszaychik <aszaychik@gmail.com>
2021-12-19 08:20:54 +0700

    init

---
+++ Python/[Tutorial]/04_[Tutorial]_Casting.py
@@ -0,0 +1,22 @@
+print("====INTEGER====")
+data_int = 10
+print("data : ", data_int,", tipe data : ",type(data_int))
+
+
+data_float = float(data_int)
+data_str= str(data_int)
+data_bool = bool(data_int)
+print("data : ", data_float,", tipe data : ",type(data_float))
+print("data : ", data_str,", tipe data : ",type(data_str))
+print("data : ", data_bool,", tipe data : ",type(data_bool))
+
+print("====FLOAT====")
+data_float = 10.5
+print("data : ", data_float,", tipe data : ",type(data_float))
+
+data_float = float(data_float)
+data_str= str(data_float)
+data_bool = bool(data_float)
+print("data : ", data_int,", tipe data : ",type(data_int))
+print("data : ", data_str,", tipe data : ",type(data_str))
+print("data : ", data_bool,", tipe data : ",type(data_bool))
